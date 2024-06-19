2a8ea9162a214bf5f5628001735c70c6657876df
alexeysamkov <alexeysamkov@gmail.com>
2024-06-20 00:39:59 +0300

    выравнивал элементы

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -3,25 +3,31 @@ import { Link } from 'react-router-dom';
 import stylesAppHeader from "./AppHeader.module.css"; 
 import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
-
 const AppHeader = () => {
     return (
         <header className={stylesAppHeader.header}>
-            <Link className={stylesAppHeader.hdlink} to="/constructor">
-            <BurgerIcon type="primary"/>
-                   <span className={stylesAppHeader.hdtext}>Конструктор</span>
-            </Link>
-            <Link className={stylesAppHeader.hdlink} to="/orders">
-                <ListIcon type="secondary" />
-                <span className={stylesAppHeader.hdtextinactive}>Лента заказов</span>
-            </Link>
-            <Link to="/" >
-                <Logo />
-            </Link>
-            <Link className={stylesAppHeader.hdlink} to="/profile">
-            <ProfileIcon type='secondary' />
-                Личный кабинет
-            </Link>
+            <div className={stylesAppHeader.section}>
+                <Link className={stylesAppHeader.hdlink} to="/constructor">
+                    <BurgerIcon type="primary"/>
+                    <span className={stylesAppHeader.hdtext}>Конструктор</span>
+                </Link>
+                <Link className={stylesAppHeader.hdlink} to="/orders">
+                    <ListIcon type="secondary" />
+                    <span className={stylesAppHeader.hdtextinactive}>Лента заказов</span>
+                </Link>
+            </div>
+            <div className={stylesAppHeader.section}>
+                <Link to="/" >
+                    <Logo />
+                </Link>
+            </div>
+            
+            <div className={stylesAppHeader.section}>
+                <Link className={stylesAppHeader.hdlink} to="/profile">
+                    <ProfileIcon type='secondary' />
+                    <span className={stylesAppHeader.hdtextinactive}>Личный кабинет</span>
+                </Link>
+            </div>
         </header>
     );
 };
