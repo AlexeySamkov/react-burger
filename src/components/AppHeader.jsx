b7834acd34209fb48f51a863ef4d3a647cd4bd90
alexeysamkov <alexeysamkov@gmail.com>
2024-06-20 08:50:42 +0300

    цвета менял

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
 import stylesAppHeader from "./AppHeader.module.css"; 
 import { BurgerIcon, ListIcon, Logo, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import React, { useState } from 'react';
 
 const AppHeader = () => {
+
+        // Создаем отдельное состояние для каждой иконки
+        const [isBurgerHovered, setIsBurgerHovered] = useState(false);
+        const [isListHovered, setIsListHovered] = useState(false);
+        const [isProfileHovered, setIsProfileHovered] = useState(false);
+
+
     return (
         <header className={stylesAppHeader.header}>
             <div className={stylesAppHeader.section}>
-                <Link className={stylesAppHeader.hdlink} to="/constructor">
-                    <BurgerIcon type="primary"/>
-                    <span className={stylesAppHeader.hdtext}>Конструктор</span>
+            <Link                     
+                    className={stylesAppHeader.link}
+                    to="/constructor"
+                    onMouseEnter={() => setIsBurgerHovered(true)} // Устанавливаем isHovered в true, когда мышь наводится на элемент
+                    onMouseLeave={() => setIsBurgerHovered(false)} // Устанавливаем isHovered в false, когда мышь покидает элемент
+                >
+                    <span className={stylesAppHeader.icon}>
+                    {isBurgerHovered ? <BurgerIcon type="primary"/> : <BurgerIcon type="secondary"/>}
+                    </span>
+                    Конструктор
                 </Link>
-                <Link className={stylesAppHeader.hdlink} to="/orders">
-                    <ListIcon type="secondary" />
-                    <span className={stylesAppHeader.hdtextinactive}>Лента заказов</span>
+                <Link 
+                    className={stylesAppHeader.link} 
+                    to="/orders"
+                    onMouseEnter={() => setIsListHovered(true)} // Устанавливаем isHovered в true, когда мышь наводится на элемент
+                    onMouseLeave={() => setIsListHovered(false)} // Устанавливаем isHovered в false, когда мышь покидает элемент
+                >    
+                <span className={stylesAppHeader.icon}>
+                    {isListHovered ? <ListIcon type="primary"/> : <ListIcon type="secondary"/>}
+                    </span>                
+                    Лента заказов
                 </Link>
             </div>
             <div className={stylesAppHeader.section}>
@@ -23,9 +45,16 @@ const AppHeader = () => {
             </div>
             
             <div className={stylesAppHeader.section}>
-                <Link className={stylesAppHeader.hdlink} to="/profile">
-                    <ProfileIcon type='secondary' />
-                    <span className={stylesAppHeader.hdtextinactive}>Личный кабинет</span>
+            <Link 
+                    className={stylesAppHeader.link} 
+                    to="/orders"
+                    onMouseEnter={() => setIsProfileHovered(true)} // Устанавливаем isHovered в true, когда мышь наводится на элемент
+                    onMouseLeave={() => setIsProfileHovered(false)} // Устанавливаем isHovered в false, когда мышь покидает элемент
+                >  
+                <span className={stylesAppHeader.icon}>
+                    {isProfileHovered ? <ProfileIcon type="primary"/> : <ProfileIcon type="secondary"/>}  
+                    </span>                                                
+                    Личный кабинет
                 </Link>
             </div>
         </header>
