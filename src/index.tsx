789406cfbc96b41e0cdbc12d6010743a856d9115
Alexey Samkov <alexeysamkov@gmail.com>
2024-08-07 10:31:56 +0300

    забыл про index.jsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,20 +9,24 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <DndProvider backend={HTML5Backend}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </DndProvider>
-    </Provider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <DndProvider backend={HTML5Backend}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </DndProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
